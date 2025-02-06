a349733f43ddec6c2766782f508b2dd85943727d
MoFune <100536617+MoFune@users.noreply.github.com>
2025-02-06 12:07:43 +0100

    Add files via upload

--- Brutti.py
+++ Brutti.py
@@ -26,7 +26,7 @@ df_filtrato["Immagine"] = df_filtrato["Immagine"].fillna("https://via.placeholde
 
 # Visualizzare la galleria di elmi
 for _, row in df_filtrato.iterrows():
-    st.image(row["Immagine"], caption=row["Nome"], use_column_width=True)
+    st.image(row["Immagine"], caption=row["Nome"], use_container_width=True)
     st.write(f"**Epoca:** {row['Epoca']} | **Provenienza:** {row['Provenienza']}")
     st.write(f"**Materiale:** {row['Materiale']}")
 
