2da92053eec5cf74cb6b7599abfda6b575a0a931
MoFune <100536617+MoFune@users.noreply.github.com>
2025-02-06 12:45:13 +0100

    Add files via upload

--- Brutti.py
+++ Brutti.py
@@ -8,14 +8,14 @@ import streamlit as st
 import pandas as pd
 
 # Caricare i dati dal CSV
-df = pd.read_csv("elmi_fixed.csv")
+df = pd.read_csv("elmi_fixed2.csv")
 
 # Titolo della pagina
 st.title("Brutti Ceffi del Met Museum")
 
 
 # Visualizzare la galleria di elmi
-for _, row in df_filtrato.iterrows():
+for _, row in df.iterrows():
     st.image(row["Immagine"], caption=row["Nome"], use_container_width=True)
     st.write(f"**Epoca:** {row['Epoca']} | **Provenienza:** {row['Provenienza']}")
     st.write(f"**Materiale:** {row['Materiale']}")
