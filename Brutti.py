77f3ff7c90d37ce7e70e356d677b517560d03a53
MoFune <100536617+MoFune@users.noreply.github.com>
2025-02-06 12:39:48 +0100

    Add files via upload

--- Brutti.py
+++ Brutti.py
@@ -13,16 +13,6 @@ df = pd.read_csv("elmi_fixed.csv")
 # Titolo della pagina
 st.title("Brutti Ceffi del Met Museum")
 
-# Filtro per epoca e provenienza
-epoche = df["Epoca"].unique().tolist()
-provenienze = df["Provenienza"].unique().tolist()
-
-filtro_epoca = st.multiselect("Filtra per epoca:", epoche, default=epoche)
-filtro_provenienza = st.multiselect("Filtra per provenienza:", provenienze, default=provenienze)
-
-# Filtrare il DataFrame
-df_filtrato = df[(df["Epoca"].isin(filtro_epoca)) & (df["Provenienza"].isin(filtro_provenienza))]
-df_filtrato["Immagine"] = df_filtrato["Immagine"].fillna("https://via.placeholder.com/300")
 
 # Visualizzare la galleria di elmi
 for _, row in df_filtrato.iterrows():
