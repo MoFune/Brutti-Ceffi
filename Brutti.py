2e10726b69c6f9fcda662c83290cd7b9e900ff15
MoFune <100536617+MoFune@users.noreply.github.com>
2025-02-07 17:11:57 +0100

    Update Brutti.py

--- Brutti.py
+++ Brutti.py
@@ -22,7 +22,7 @@ st.markdown(
         box-shadow: 2px 2px 10px rgba(0,0,0,0.2);
         padding: 10px;
         margin-bottom: 20px;
-        background-color: gray;
+        background-color:light gray;
     }
     .elmo-image {
         border-radius: 8px;
